b5327b29de6c5c608ba97615d2745ce81dee9ccd
Kingbloxxa <jan.matejovsky2009@gmail.com>
2025-07-08 15:47:12 +0200

    sans

---
+++ Program.cs
@@ -0,0 +1,169 @@
+﻿
+using System.Collections.Generic;
+
+/*
+Console.Write("gimme a number");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0) {
+    Console.WriteLine("tvoje cislo is sude");
+}
+else { Console.WriteLine("tvoje cislo je liche"); }
+
+
+
+
+Console.Write("Kolik ti je let?");
+int věk = int.Parse(Console.ReadLine());
+if (věk < 13) {
+    Console.WriteLine("GOOO GOOO GAAAA GAAAA");
+}
+
+else if (věk > 13 & věk < 18) {
+    Console.WriteLine("STOP PLAYING GAMES BOZO");
+}
+
+else Console.WriteLine("Get a job unc");
+
+
+
+Console.Write("1 numma");
+int a = int.Parse(Console.ReadLine());
+Console.Write("2 numma");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Alr lemme cook bro ");
+int deez = a + b;
+int nuts = a - b;
+int uhhhhhh = a * b;
+int iForgor = a / b;
+Console.WriteLine("scitani " + deez);
+Console.WriteLine("odcitani " + nuts);
+Console.WriteLine("nasobeni " + uhhhhhh);
+Console.WriteLine("deleni " + iForgor);
+
+Console.Write("1 numma des. cislo btw");
+float a = float.Parse(Console.ReadLine());
+Console.Write("2 numma des. cislo btw");
+float b = float.Parse(Console.ReadLine());
+Console.Write("3 numma des. cislo btw");
+float c = float.Parse(Console.ReadLine());
+Console.WriteLine("Alr lemme cook bro ");
+float vysledek = a + b + c;
+Console.Write("alr bro here it is " + vysledek/3);
+
+
+Console.WriteLine("whats ur name big boy?");
+string name = Console.ReadLine();
+Console.WriteLine("alr " + name + " kys");
+
+
+
+
+int score = 0;
+
+score += 10;
+
+Console.WriteLine(score);
+
+score *= 2;
+
+Console.WriteLine(score);
+
+score -= 5;
+
+Console.WriteLine(score);
+
+int a = 12; // 1100
+int b = 5; // 0101
+int vysledek1 = a & b;
+Console.WriteLine(vysledek1);
+int vysledek2 = a | b;
+Console.WriteLine(vysledek2);
+int vysledek3 = a ^ b;
+Console.WriteLine(vysledek3);
+int vysledek4 = a << 1;
+Console.WriteLine(vysledek4);
+int vysledek5 = a >> 1;
+Console.WriteLine(vysledek5);
+
+
+int a = 10;
+int b = 7;
+bool real = a > b;
+Console.WriteLine(real);
+bool realreal = a <= b;
+Console.WriteLine(realreal);
+bool realrealreal = a == b;
+Console.WriteLine(realrealreal);
+
+
+Console.WriteLine("Zadejte svůj věk... ");
+string věk = Console.ReadLine();
+Console.WriteLine("Zadal jste " + věk);
+bool ok = int.TryParse(věk, out int věk2);
+int věk3 = 18; 
+if (věk2 >= 18){ 
+    
+    Console.WriteLine("Je vám / více než " + věk3);
+}
+  else if(ok == false) { 
+    Console.WriteLine("Nezadali jste číslo... ");
+}
+
+else {
+    Console.WriteLine("Nejste dost staří");
+
+}
+
+
+
+
+
+Console.WriteLine("Jak se jmenujete?");
+string jmeno = Console.ReadLine();   
+Console.WriteLine(" a kolik je vam let?");
+string věk = Console.ReadLine(); 
+Console.WriteLine($"Ahoj, {jmeno} je ti {věk} let");
+
+
+string[] měsíce = {"leden", "unor","březen", "duben", "květen", "červen", "červenec", "srpen", "září", "listopad", "prosinec"};
+int[]dny = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+Console.WriteLine("Červenec je " + měsíce[6] + " měsíc a má " + dny[6] + " dnů");
+Console.WriteLine("Listopad je " + měsíce[10] + " měsíc a má " + dny[10] + " dnů");
+
+
+List<int> numbers  = new List<int> { 100, 200, 300 };
+numbers.Remove(200);
+numbers.Add(400);
+Console.WriteLine(numbers[0]);
+Console.WriteLine(numbers[1]);
+Console.WriteLine(numbers[2]);
+int kolik = numbers.Count;
+Console.WriteLine(kolik);
+
+
+string[] zvirata = {"pes", "kocka", "ryba"};
+
+for (int i = 0; i < 3; i++ ) {
+    Console.WriteLine(zvirata[i]);
+}
+*/
+/*
+string[] funny = {}
+
+foreach( string lol in funny ){
+    Console.WriteLine(lol);
+    Thread.Sleep(1000);
+}
+*/
+/*
+Console.WriteLine("Gimme a number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("another...");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("AND ANOTHER");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine("ur nummers are " + a + b + c);
+*/
+
+
+
